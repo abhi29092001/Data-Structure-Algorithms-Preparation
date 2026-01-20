17a353ca1bd0fefd33667f533b3b1d8ea2f32a39
Abhishek Sharma <sharmaabh458@gmail.com>
2026-01-20 22:47:11 +0530

    Added optimal solution of longest substring with k uniques

---
+++ Sliding-Window-Approach/Longest-Substring-With-K-Uniques/Optimal.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+public class Optimal {
+    public static void main(String[] args) {
+        String s = "aabacbebebe";
+        int k = 3;
+        System.out.println(solution(s, k));
+    }
+
+    public static int solution(String s, int k) {
+        int size = s.length();
+        HashMap<Character, Integer> freq = new HashMap<>();
+        int maxLen = -1;
+        int low = 0;
+        
+        for (int high = 0; high < size; high++) {
+            freq.put(s.charAt(high), freq.getOrDefault(s.charAt(high), 0) + 1);
+            
+            while (freq.size() > k) {
+                freq.put(s.charAt(low), freq.get(s.charAt(low)) - 1);
+                
+                if (freq.get(s.charAt(low)) == 0) {
+                    freq.remove(s.charAt(low));
+                }
+                
+                low++;
+            }
+            
+            if (freq.size() == k) {
+                maxLen = Math.max(maxLen, high - low + 1);
+            }
+        }
+        
+        return maxLen;
+    }
+}
