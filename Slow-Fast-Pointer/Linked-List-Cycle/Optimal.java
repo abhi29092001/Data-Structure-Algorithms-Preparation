78e1f814d3cfb9f1347b6bb1dad91dd226ffa756
Abhishek Sharma <sharmaabh458@gmail.com>
2026-02-05 16:20:36 +0530

    Added optimal solution of linked list cycle

---
+++ Slow-Fast-Pointer/Linked-List-Cycle/Optimal.java
@@ -0,0 +1,27 @@
+class ListNode {
+    int data;
+    ListNode next;
+    
+    ListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Optimal {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
