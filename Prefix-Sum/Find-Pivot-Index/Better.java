10f2936cd9ed585e50fca6f6e10b9edaa38f25e5
Abhishek Sharma <sharmaabh458@gmail.com>
2026-02-18 15:11:05 +0530

    Added brute force solution of subarray sum equals k

--- Prefix-Sum/Find-Pivot-Index/Better.java
+++ Prefix-Sum/Find-Pivot-Index/Better.java
@@ -1,7 +1,7 @@
 // TC -> O(n)
 // SC -> O(n)
 
-public class Best {
+public class Better {
     public static void main(String[] args) {
         int[] nums = {2, -1, 1};
         System.out.println(pivotIndex(nums));
---
+++ Prefix-Sum/Subarray-Sum-Equals-K/Brute.java
@@ -0,0 +1,29 @@
+// TC -> O(n^2)
+// SC -> O(1)
+
+public class Brute {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1};
+        int k = 2;
+        System.out.println(subarraySum(nums, k));
+    }
+
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
