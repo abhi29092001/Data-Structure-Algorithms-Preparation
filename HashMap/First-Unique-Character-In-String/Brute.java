946b50a1e94fa9618c8955032ba1b8a397bcfe93
Abhishek Sharma <sharmaabh458@gmail.com>
2026-03-11 22:25:12 +0530

    Added brute force solution of first unique character in a string

---
+++ HashMap/First-Unique-Character-In-String/Brute.java
@@ -0,0 +1,32 @@
+public class Brute {
+    public static void main(String[] args) {
+        String s = "abbccdda";
+        System.out.println(firstUniqChar(s));
+    }
+
+    public static int firstUniqChar(String s) {
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            // boolean variable to keep a track of whether character is unique or not
+            boolean uniqueFound = true;
+
+            for (int j = 0; j < n; j++) {
+                // for every character inner loop will run from starting to properly check
+                // take this test case - abcda
+                // if we don't run loop from starting in every iteration then code will fail
+                if (i != j && s.charAt(i) == s.charAt(j)) {
+                    uniqueFound = false;
+                    break;
+                }
+            }
+
+            // if we don't find any duplicate then return ith index, otherwise -1
+            if (uniqueFound) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
