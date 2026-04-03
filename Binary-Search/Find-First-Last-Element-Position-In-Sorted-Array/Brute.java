8e5ade596c5ca180f35578ea0ac0250c5d1a3cf6
Abhishek Sharma <sharmaabh458@gmail.com>
2026-04-03 17:51:36 +0530

    Added time and space complexity

--- Binary-Search/Find-First-Last-Element-Position-In-Sorted-Array/Brute.java
+++ Binary-Search/Find-First-Last-Element-Position-In-Sorted-Array/Brute.java
@@ -1,3 +1,6 @@
+// TC -> O(n)
+// SC -> O(1)
+
 public class Brute {
     public static void main(String[] args) {
         int[] nums = {5,7,7,8,8,10};
@@ -9,10 +12,12 @@ public class Brute {
         // Logic-
         // 1. In first iteration get the starting index of the target and break
         // 2. In second iteration get the last index of the traget, if there is only one occurrence of traget then also you will get atleast starting and ending index although it will be same in that case
-        // 3. if no occurrence then -1 would be returned
+        // 3. if no occurrence then -1 would be returned which is initialized at starting
         
         int n = nums.length;
         int[] range = new int[2];
+
+        // Initialize array with -1
         range[0] = -1;
         range[1] = -1;
 
