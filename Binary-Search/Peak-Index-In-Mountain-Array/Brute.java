707a83e506653529c55de89b9c7ad4cf612d6004
Abhishek Sharma <sharmaabh458@gmail.com>
2026-04-04 16:06:48 +0530

    Added brute force solution of peak index in mountain array

---
+++ Binary-Search/Peak-Index-In-Mountain-Array/Brute.java
@@ -0,0 +1,24 @@
+public class Brute {
+    public static void main(String[] args) {
+        int[] arr = {0,1,0};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+    
+    public static int peakIndexInMountainArray(int[] arr) {
+        // Logic-
+        // Simple linear search approach
+        
+        int n = arr.length;
+        int peak = -1;
+        int peakIndex = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > peak) {
+                peak = arr[i];
+                peakIndex = i;
+            }
+        }
+
+        return peakIndex;
+    }
+}
