2050a359180b0dcc6aa953de81ce2d0c14d6efc3
Abhishek Sharma <sharmaabh458@gmail.com>
2026-05-18 21:51:54 +0530

    Added brute force solution of search in rotated sorted array

---
+++ Binary-Search/Search-In-Rotated-Sorted-Array/Brute.java
@@ -0,0 +1,22 @@
+public class Brute {
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(search(nums, target));
+    }
+
+    public static int search(int[] nums, int target) {
+        // Logic-
+        // Simple Linear Search
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
