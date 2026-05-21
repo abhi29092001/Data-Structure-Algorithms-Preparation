b7d8a4e79a780a6856080f354600783270a7f611
Abhishek Sharma <sharmaabh458@gmail.com>
2026-05-21 23:05:54 +0530

    Added brute force solution of aggressive cows

---
+++ Binary-Search/Aggressive-Cows/Brute.java
@@ -0,0 +1,62 @@
+// TC -> O(n * m)
+// SC -> O(1)
+
+import java.util.*;
+
+public class Brute {
+    public static void main(String[] args) {
+        int[] stalls = {1, 2, 4, 8, 9};
+        int k = 3;
+        System.out.println(aggressiveCows(stalls, k));
+    }   
+    
+    public static int aggressiveCows(int[] stalls, int k) {
+        // Logic - 
+        // 1. we have to maximise the min distance between any two cows
+        // 2. first sort the stalls (It'll be easy if stalls are sorted)
+        // 3. min dist will be 1 between any two cows and that is low
+        // 4. max dist will be diff between first stall and last stall and that is high
+        // 5. you have to make a guess for dist
+        // 6. pass that guess to helper func
+        // 7. helper func will place cows in stalls and check if the min dist is greater or equal to min dist, if yes then cows will be incremented that means cow has been placed
+        // 8. Atlast check if all cows placed or not and similarly return true or false based on the check
+        // 9. if true is returned that means we have to keep going on till we get min dist at which we cant place all cows 
+        // 10. if false then stop loop and get out of it
+
+        int n = stalls.length;
+        int result = -1;
+        
+        Arrays.sort(stalls);
+        
+        int low = 1;
+        int high = stalls[n - 1] - stalls[0];
+        
+        for (int i = low; i <= high; i++) {
+            if (helper(stalls, n, i, k)) {
+                result = i;
+            } else {
+                break;
+            }
+        }
+        
+        return result;
+    }
+
+    public static boolean helper(int[] stalls, int n, int minDist, int k) {
+        int cows = 1;
+        int prevCow = stalls[0];
+        
+        for (int i = 1; i < n; i++) {
+            if (stalls[i] - prevCow >= minDist) {
+                cows++;
+                prevCow = stalls[i];
+            }
+        }
+        
+        if (cows >= k) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
