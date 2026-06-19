d33d8877cc3a67776d68e4f5f28fef8564f2ae46
Abhishek Sharma <sharmaabh458@gmail.com>
2026-06-19 08:15:58 +0530

    Added some explanation in optimal solution of linked list cycle

--- Slow-Fast-Pointer/Linked-List-Cycle/Optimal.java
+++ Slow-Fast-Pointer/Linked-List-Cycle/Optimal.java
@@ -13,6 +13,11 @@ class ListNode {
 
 public class Optimal {
     public boolean hasCycle(ListNode head) {
+        // Logic-
+        // Take two pointers, one will move one step at a time and other will move two steps at a time
+        // if there's a cycle then they will surely meet at some point of time, return true
+        // if not then fast will reach at the end and come out of loop, false will be returned
+
         ListNode slow = head;
         ListNode fast = head;
 
