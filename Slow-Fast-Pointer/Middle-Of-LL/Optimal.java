17d72968d2217505d0823953f16a8f8cd95e583d
Abhishek Sharma <sharmaabh458@gmail.com>
2026-06-19 09:01:22 +0530

    Added explanation to optimal solution of middle of LL

--- Slow-Fast-Pointer/Middle-Of-LL/Optimal.java
+++ Slow-Fast-Pointer/Middle-Of-LL/Optimal.java
@@ -13,6 +13,12 @@ class ListNode {
 
 public class Optimal {
     public ListNode middleNode(ListNode head) {
+        // Logic -
+        // to find middle of LL, take two pointers
+        // slow will move 1 step and fast will move 2 step
+        // run loop till fast is not null and its next is also not null
+        // if either of the condition becomes false, return slow because slow is exactly standing at middle node 
+
         ListNode slow = head;
         ListNode fast = head;
 
