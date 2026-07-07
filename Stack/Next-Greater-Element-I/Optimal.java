df187c93316d4086a4c88b2b551d2aea9e0be56f
Abhishek Sharma <sharmaabh458@gmail.com>
2026-07-07 09:19:16 +0530

    Added explanation to optimal solution of next greater element-I

--- Stack/Next-Greater-Element-I/Optimal.java
+++ Stack/Next-Greater-Element-I/Optimal.java
@@ -11,6 +11,11 @@ public class Optimal {
     }
     
     public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // Logic-
+        // stack will be used in this ques as ques is asking for next greater element
+        // will use stack and check whether the top element is lesser than the current element, if so that means top element's greater element is current element and store it in hashmap for further use
+        // once done now loop on 1st array to find out greater elements for elements lying in array1, use hashmap for lookup
+
         // to check whether top is lesser than current element or not
         Stack<Integer> st = new Stack<>();
 
@@ -19,6 +24,7 @@ public class Optimal {
 
         int[] resArray = new int[nums1.length];
 
+        // Buidling monotonic stack and storing key-value pair in map 
         for (int j = 0; j < nums2.length; j++) {
             while (!st.isEmpty() && st.peek() < nums2[j]) {
                 map.put(st.pop(), nums2[j]);
